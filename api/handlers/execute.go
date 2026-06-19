03fd3e0dea8ca3dc1bb0547dd17cf574fe977592
Sukanthan06 <su.sukanthan@gmail.com>
2026-06-19 13:16:30 +0530

    feat(api): add execute endpoint and request models

--- api/handlers/execute.go
+++ api/handlers/execute.go
@@ -1,1 +1,25 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/Sukanthan06/code-execution-engine/models"
+)
+
+func ExecuteCode(c *gin.Context) {
+	var req models.ExecuteRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"language": req.Language,
+		"code":     req.Code,
+	})
+}
