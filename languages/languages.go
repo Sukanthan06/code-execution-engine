848e5c74305f41edf5c491ef68b013b94ce09f20
Sukanthan06 <su.sukanthan@gmail.com>
2026-06-24 22:46:52 +0530

    refactor:add language resgistry

---
+++ languages/languages.go
@@ -0,0 +1,16 @@
+package languages
+
+type Language struct {
+	Name      string
+	Extension string
+	Command   string
+}
+
+// language resgistry
+var SupportedLanguages = map[string]Language{
+	"python": {
+		Name:      "Python",
+		Extension: ".py",
+		Command:   "python",
+	},
+}
