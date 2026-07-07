d12d637158c7125039745aaf848e1704e0e46598
Sukanthan06 <su.sukanthan@gmail.com>
2026-07-07 23:57:48 +0530

    feat:add C language execution support

--- executor/sandbox/runner.go
+++ executor/sandbox/runner.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"time"
 )
 
-func RunCode(code string, command string, extension string) (string, error) {
+func RunCode(code string, extension string, interpreter string, compiler string) (string, error) {
 	tmpFile, err := os.CreateTemp("", "*"+extension)
 	if err != nil {
 		return "", err
@@ -18,19 +19,42 @@ func RunCode(code string, command string, extension string) (string, error) {
 	if _, err := tmpFile.WriteString(code); err != nil {
 		return "", err
 	}
-
 	tmpFile.Close()
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+	// for interpreted languages
+	if interpreter != "" {
 
-	cmd := exec.CommandContext(ctx, command, tmpFile.Name())
+		cmd := exec.CommandContext(ctx, interpreter, tmpFile.Name())
 
-	output, err := cmd.CombinedOutput()
+		output, err := cmd.CombinedOutput()
+
+		if ctx.Err() == context.DeadlineExceeded {
+			return "", fmt.Errorf("execution timed out after 5 seconds")
+		}
+
+		return string(output), err
+	}
+	// compiled languages
+	executable := filepath.Join(os.TempDir(), fmt.Sprintf("%d.exe", time.Now().UnixNano()))
+
+	defer os.Remove(executable)
+	//compile
+	compilecmd := exec.CommandContext(ctx, compiler, tmpFile.Name(), "-o", executable)
+
+	compileOutput, err := compilecmd.CombinedOutput()
+
+	if err != nil {
+		return string(compileOutput), err
+	}
+	//execute
+	runCmd := exec.CommandContext(ctx, executable)
+
+	runOutput, err := runCmd.CombinedOutput()
 
 	if ctx.Err() == context.DeadlineExceeded {
 		return "", fmt.Errorf("execution timed out after 5 seconds")
 	}
 
-	return string(output), err
-
+	return string(runOutput), err
 }
--- languages/languages.go
+++ languages/languages.go
@@ -1,21 +1,27 @@
 package languages
 
 type Language struct {
-	Name      string
-	Extension string
-	Command   string
+	Name        string
+	Extension   string
+	Interpreter string
+	Compiler    string
 }
 
 // language resgistry
 var SupportedLanguages = map[string]Language{
 	"python": {
-		Name:      "Python",
-		Extension: ".py",
-		Command:   "python",
+		Name:        "Python",
+		Extension:   ".py",
+		Interpreter: "python",
 	},
 	"javascript": {
-		Name:      "JavaScript",
-		Extension: ".js",
-		Command:   "node",
+		Name:        "JavaScript",
+		Extension:   ".js",
+		Interpreter: "node",
+	},
+	"c": {
+		Name:      "C",
+		Extension: ".c",
+		Compiler:  "gcc",
 	},
 }
