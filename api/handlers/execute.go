0fb02abaf0c75ffc490ab2482d0febcc62a8551d
Sukanthan06 <su.sukanthan@gmail.com>
2026-08-02 22:21:01 +0530

    feat: execute C++ inside Docker container

--- api/handlers/execute.go
+++ api/handlers/execute.go
@@ -31,7 +31,7 @@ func ExecuteCode(c *gin.Context) {
 	var output string
 	var err error
 
-	if req.Language == "python" || req.Language == "javascript" || req.Language == "c" {
+	if req.Language == "python" || req.Language == "javascript" || req.Language == "c" || req.Language == "cpp" {
 		output, err = dockerrunner.RunCode(
 			req.Code,
 			lang.Extension,
